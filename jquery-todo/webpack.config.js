8c316f5ee8d8e2fbd323b52c857ebb3d945021c0
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-04-25 02:43:26 +0900

    jquery: delete: いらないの削除

--- jquery-todo/webpack.config.js
+++ jquery-todo/webpack.config.js
@@ -1,18 +1,15 @@
 const path = require('path');
 module.exports = {
-  // モジュールバンドルを行う起点となるファイルの指定
-  // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
-  // 下記はオブジェクトとして指定した例
+
   entry: {
     bundle: './src/index.js'
   },
-  // モジュールバンドルを行った結果を出力する場所やファイル名の指定
   output: {
-    path: path.join(__dirname, 'dist'), // "__dirname"はファイルが存在するディレクトリ
-    filename: '[name].js'  // [name]はentryで記述した名前（この設定ならbundle）
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'), // webpack-dev-serverの公開フォルダ
-    open: false // サーバー起動時にブラウザを開く
+    contentBase: path.join(__dirname, 'dist'),
+    open: false
   },
 }
