be94ae4c982df809dc252bacf2f9c84e94736ea8
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-04-25 23:11:26 +0900

    vue-todo: fix: サインアウト処理直呼び

--- vue-todo/src/components/HeaderLinks.vue
+++ vue-todo/src/components/HeaderLinks.vue
@@ -2,15 +2,29 @@
   <ul class="tab-menu-base">
     <li><router-link to="/">home</router-link></li>
     <li><router-link to="/todos">todos</router-link></li>
-    <!-- <li v-model="">sign_out</li> -->
+    <li><a class="signout" v-on:click="handleSignOut">sign_out</a></li>
   </ul>
 </template>
 <script>
+import { auth } from '../db'
+
 export default {
   name: 'HeaderLinks',
   methods:{
-    signOut() {
+    handleSignOut() {
+      if(!confirm("サインアウトしちゃうぞ")) return
+      auth.signOut().then(() => {
+        this.$router.push('sign_in')
+      })
     }
   }
 }
 </script>
+<style>
+
+.signout{
+  cursor: pointer;
+  color: -webkit-link;
+  text-decoration: underline;
+}
+</style>
