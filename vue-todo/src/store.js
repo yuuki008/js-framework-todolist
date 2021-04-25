9af0b093282ea7797416309bb288c623f9c41866
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-04-25 23:10:57 +0900

    fix: とりあえず今は使わなから、、、コメントアウト

--- vue-todo/src/store.js
+++ vue-todo/src/store.js
@@ -1,24 +1,67 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+// import { db } from '../../jquery-todo/src/db'
+// import { auth } from './db'
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
 
-const store = new Vuex.Store({
-  state: {
-    isSignIn: false,
-    uid: ""
-  },
-  mutations: {
-    updateUser(state, user) {
-      state.uid = user.uid
-      state.isSignIn = true
-    }
-  },
-  actions: {
-    auth(context, user) {
-      context.commit('updateUser', user)
-    }
-  }
-})
+// const store = new Vuex.Store({
+//   state: {
+//     isSignIn: false,
+//     uid: ""
+//   },
+//   getters: {
+//     isSignIn: (state) => state.isSignIn
+//   },
+//   mutations: {
+//     sign_out_mutation(state) {
+//       state.isSignIn = false,
+//       state.uid = ""
+//     },
+//     sign_in_mutation(state, { id: id }) {
+//       state.isSignIn = true
+//       state.uid = id
+//     }
+//   },
+//   actions: {
+//     sign_in({ commit },{ email, password } ) {
+//       console.log(email, password)
+//       auth.signInWithEmailAndPassword(email, password)
+//       .then((result) => {
+//         const user = result.user
+//         if (user) {
+//           const id = user.uid
+//           db.collection('users').doc(id).get().then((snapshot) => {
+//             commit('sign_in_mutation', { id: id })
+//           })
+//         }
+//       })
+//     },
+//     sign_out({ commit }) {
+//       auth.signOut()
+//       .then(() => {
+//         console.log('サインアウト成功')
+//         commit('sign_out_mutation')
+//       })
+//     },
+//     listen_auth({ commit }) {
+//       auth.onAuthStateChanged((user) => {
+//         if (user) {
+//           const id = user.uid
+//           db.collection('users').doc(id).get().then((snapshot) => {
+//             const data = snapshot.data()
+//             if (!data) {
+//               commit('sign_out_mutation')
+//               return
+//             }
+//             commit('sign_in_mutation', { id: data.id })
+//           })
+//         } else {
+//           commit('sign_out_mutation')
+//         }
+//       })
+//     }
+//   }
+// })
 
-export default store
+// export default store
