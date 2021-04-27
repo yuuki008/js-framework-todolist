1056260aea29a4e156d2532bb513a9afea3a621e
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-04-27 14:03:14 +0900

    add: guard追加

---
+++ angular-todo/src/app/guard/auth.guard.ts
@@ -0,0 +1,33 @@
+  import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { SessionService } from '../service/session.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private session: SessionService,
+    private router: Router
+  ){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean>
+  {
+    return this.session
+      .checkLoginState()
+      .pipe(
+        map(session => {
+          if (!session.login) {
+            ;
+            this.router.navigate(['/account/sign_in']);
+          }
+          return session.login;
+        })
+      )
+  }
+}
