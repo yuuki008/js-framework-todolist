1cdbf052d76bce7f349d73c6b531d916c7c96759
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-05-02 13:45:45 +0900

    fix: ルーティング修正

--- react-todo/src/index.jsx
+++ react-todo/src/index.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import {render} from 'react-dom';
 import * as History from 'history'
-import { Route, Switch } from 'react-router'
-import { ConnectedRouter } from 'connected-react-router'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+// import { ConnectedRouter } from 'connected-react-router'
 import { SignUp } from './pages/SignUp';
 import { SignIn } from './pages/SignIn';
 import { Todo } from './pages/Todo';
 import { AuthWrapper } from './AuthWrapper'
 
-const history = History.createBrowserHistory()
 
 render(
-  <ConnectedRouter history={history}>
-    <Switch>
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/signin" component={SignIn} />
-      <AuthWrapper>
-        <>
-          <Route exact path="/" component={Todo} />
-        </>
-      </AuthWrapper>
-    </Switch>
-  </ConnectedRouter>,
+    <Router>
+      <Switch>
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <AuthWrapper>
+          <>
+            <Route exact path="/" component={Todo} />
+          </>
+        </AuthWrapper>
+      </Switch>
+    </Router>,
   document.getElementById('root')
 );
 
