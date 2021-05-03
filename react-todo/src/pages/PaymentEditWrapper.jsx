94a8720a128312eaea1d597b9a9a7ee90ca5e8a3
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-05-03 22:17:18 +0900

    fix: jsx化

--- react-todo/src/pages/PaymentEditWrapper.jsx
+++ react-todo/src/pages/PaymentEditWrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { loadStripe } from '@stripe/stripe-js/pure'
 import { Elements } from '@stripe/react-stripe-js'
-// import { PaymentEdit } from '../compon'
+import { PaymentEdit } from '../components/PaymentEdit'
 
 const STRIPE_PUBLIC_KEY = "pk_test_51ImD5GB8X8CGc8BDF73kN7CxlmzRmOds4alnZIQyJ9vNvl4KYcrErNfeuR8TtL5A2sVlkRof24ZGsom2vNVu6XKw00zCjXOEnJ"
 const stripePromise = loadStripe(STRIPE_PUBLIC_KEY)
